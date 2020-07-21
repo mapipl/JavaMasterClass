2c5783797aa12480d315f9632adce9ed7867ba43
Maciek P <maciek_p@interia.pl>
2020-07-21 22:27:30 +0200

    OOP encapsulation exercise part 2

---
+++ src/main/java/oop_composition/Ceiling.java
@@ -0,0 +1,19 @@
+package oop_composition;
+
+public class Ceiling {
+    private int hight;
+    private int paintedColor;
+
+    public Ceiling(int hight, int paintedColor) {
+        this.hight = hight;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHight() {
+        return hight;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
